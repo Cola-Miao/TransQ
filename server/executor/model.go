3c888248096e20c1490bd7a7bf1ea3ddd699220f
lijiashuo <lijiashuo_wk@outlook.com>
2024-09-03 20:45:03 +0800

    [server.exec] add private models

---
+++ server/executor/model.go
@@ -0,0 +1,27 @@
+package executor
+
+import (
+	"errors"
+	. "github.com/Cola-Miao/TransQ/server/models"
+)
+
+type handler func(data string) error
+
+const (
+	methodEcho = iota + 1
+)
+
+var exec executor
+
+var (
+	errNoMethod = errors.New("no method")
+)
+
+type executor struct {
+	handle map[Method]handler
+	name   map[Method]string
+}
+
+type echoRequest struct {
+	Message string `json:"msg"`
+}
