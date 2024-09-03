1f08181621b7be58d8e81f2d004bd1c7a44aa13e
lijiashuo <lijiashuo_wk@outlook.com>
2024-09-03 20:26:36 +0800

    [server.format] add more format log func

--- server/format/format.go
+++ server/format/format.go
@@ -2,10 +2,18 @@ package format
 
 import "log"
 
-func FuncStart(s string) {
-	log.Printf("%s start\n", s)
+func FuncStart(funcName string) {
+	log.Printf("%s start\n", funcName)
 }
 
-func FuncEnd(s string) {
-	log.Printf("%s end\n", s)
+func FuncEnd(funcName string) {
+	log.Printf("%s end\n", funcName)
+}
+
+func FuncStartWithData(funcName string, data any) {
+	log.Printf("%s start with data: %+v\n", funcName, data)
+}
+
+func FuncEndWithData(funcName string, data any) {
+	log.Printf("%s end with data: %+v\n", funcName, data)
 }
