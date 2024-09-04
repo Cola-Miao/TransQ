41db6ae50c1280cb205d9fd6d944caeae5267edf
lijiashuo <lijiashuo_wk@outlook.com>
2024-09-04 15:17:14 +0800

    [srv.mds] add new model for expand conn info

--- server/models/models.go
+++ server/models/models.go
@@ -1,8 +1,16 @@
 package models
 
+import "net"
+
 type Method uint8
 
 type Information struct {
 	Method Method `json:"mtd"`
 	Data   string `json:"dat"`
+}
+
+type TransQClient struct {
+	Conn net.Conn
+	Addr string
+	ID   string
 }
