99cf24a479923ef747f1b6e078eebf43729f348b
lijiashuo <lijiashuo_wk@outlook.com>
2024-09-05 16:46:17 +0800

    [srv.mds] add errors & add rwm for executor

--- server/executor/model.go
+++ server/executor/model.go
@@ -3,6 +3,7 @@ package executor
 import (
 	"errors"
 	"net"
+	"sync"
 )
 
 type handler func(tqc *transQClient) error
@@ -16,10 +17,13 @@ const (
 var exec executor
 
 var (
-	errNoMethod = errors.New("no method")
+	errNoMethod   = errors.New("no method")
+	errIDNotExist = errors.New("id not exist")
+	errIDExist    = errors.New("id exist")
 )
 
 type executor struct {
+	mu     sync.Mutex
 	handle map[method]handler
 	name   map[method]string
 	conn   map[int]net.Conn
