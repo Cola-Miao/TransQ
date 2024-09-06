1a6518b0dd061a2b46d58db116977b79672b4467
lijiashuo <lijiashuo_wk@outlook.com>
2024-09-06 13:41:14 +0800

    [srv.cch] print log when cache init

--- server/cache/cache.go
+++ server/cache/cache.go
@@ -2,12 +2,16 @@ package cache
 
 import (
 	. "github.com/Cola-Miao/TransQ/server/config"
+	"github.com/Cola-Miao/TransQ/server/format"
 	"github.com/patrickmn/go-cache"
 )
 
 var Cache *cache.Cache
 
 func InitCache() error {
+	format.FuncStart("InitCache")
+	defer format.FuncEnd("InitCache")
+
 	c := cache.New(Cfg.Cache.DefaultExpiration, Cfg.Cache.CleanupInterval)
 	Cache = c
 
