6447706841d10f55f4713ed7b5dafe47aa526059
lijiashuo <lijiashuo_wk@outlook.com>
2024-09-06 13:31:04 +0800

    [srv.cfg] add cache config model

--- server/config/model.go
+++ server/config/model.go
@@ -8,9 +8,15 @@ var (
 
 type config struct {
 	Listener listener `yaml:"listener"`
+	Cache    cache    `yaml:"cache"`
 	LogLevel int      `yaml:"logLevel"`
 }
 
 type listener struct {
 	Timeout time.Duration `yaml:"timeout"`
+}
+
+type cache struct {
+	DefaultExpiration time.Duration `yaml:"defaultExpiration"`
+	CleanupInterval   time.Duration `yaml:"cleanupInterval"`
 }
