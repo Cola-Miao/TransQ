fe49423c00c595fae88644c4d18f1e771d152c4e
lijiashuo <lijiashuo_wk@outlook.com>
2024-09-07 11:17:43 +0800

    [srv.mdl] add new errors

--- server/models/errors.go
+++ server/models/errors.go
@@ -3,11 +3,13 @@ package models
 import "errors"
 
 var (
-	ErrNoMethod       = errors.New("no method")
-	ErrIDNotExist     = errors.New("id not exist")
-	ErrIDExist        = errors.New("id exist")
-	ErrNoStructure    = errors.New("no structure")
-	ErrNoHandler      = errors.New("no handler")
-	ErrNoName         = errors.New("no name")
-	ErrBadRequestType = errors.New("bad request type")
+	ErrNoMethod            = errors.New("no method")
+	ErrIDNotExist          = errors.New("id not exist")
+	ErrIDExist             = errors.New("id exist")
+	ErrNoStructure         = errors.New("no structure")
+	ErrNoHandler           = errors.New("no handler")
+	ErrNoName              = errors.New("no name")
+	ErrBadRequestType      = errors.New("bad request type")
+	ErrUnsupportedLanguage = errors.New("unsupported language")
+	ErrAssertionType       = errors.New("wrong assertion type")
 )
